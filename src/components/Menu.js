14a6f0c8a8fb53678c2774d7b7d9405be0a0636c
Olivier Pham Dang <ophamdang@gmail.com>
2017-12-01 08:57:20 +0100

    working on responsiveness

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,14 +6,36 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+const ROUTES = {
+    HOME: '/',
+    ABOUT: '/about',
+    VISIT: '/visit',
+};
+
 class Menu extends React.Component {
     constructor(props) {
         super(props);
         
         this.state = {
-            isActive: false
+            isActive: false,
+            activeRoute: '', // init at home path
         };
         this.toggleMenu = this.toggleMenu.bind(this);
+    }
+
+    componentWillMount() {
+        const routeKeys = Object.keys(ROUTES);
+        const pathName = window.location.pathname;
+        if (this.state.activeRoute !== pathName) {
+            for (let i = 0; i < routeKeys.length; i += 1) {
+                if (pathName === ROUTES[routeKeys[i]]) {
+                    this.setState(
+                        {activeRoute: ROUTES[routeKeys[i]]},
+                        () => { console.info('> active route', this.state.activeRoute); }
+                    );
+                }
+            }
+        }
     }
 
     toggleMenu() {
@@ -23,11 +45,12 @@ class Menu extends React.Component {
             console.log(this.state.isActive);
         });
     }
-    
+
     render() {
         console.info('-->> Menu renders', this.props);
+        const {isActive, activeRoute} = this.state;
         return (
-            <div className={`${this.state.isActive ? 'menu active' : 'menu'}`}>
+            <div className={`${isActive ? 'menu active' : 'menu'}`}>
                 <div className="overlay" />
                 <div className="burger" onClick={this.toggleMenu}>
                     <div className="burger-layers">
@@ -36,10 +59,16 @@ class Menu extends React.Component {
                         <div className="burger-layer" />
                     </div>
                 </div>
-                <ul className={`${this.state.isActive ? 'links active' : 'links'}`}>
-                    <li className="home"><Link to="/">Home</Link></li>
-                    <li className="contact"><Link to="/about">About</Link></li>
-                    <li className="visit"><Link to="/visit">Visit</Link></li>
+                <ul className={`${isActive ? 'links active' : 'links'}`}>
+                    <li className={`home${activeRoute === '/' ? ' active' : ''}`}>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className={`about${activeRoute === ROUTES.ABOUT ? ' active' : ''}`}>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li className={`visit${activeRoute === ROUTES.VISIT ? ' active' : ''}`}>
+                        <Link to="/visit">Visit</Link>
+                    </li>
                 </ul>
             </div>
         );
